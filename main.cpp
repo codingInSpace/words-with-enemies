d8c7e8ada06cbed6f07aef19f2eb9c3afffa9235
Jonathan <jonathan.grangien@gmail.com>
2015-02-27 16:30:15 +0100

    Fix word collision and letter counting

--- main.cpp
+++ main.cpp
@@ -63,26 +63,56 @@ public:
 		cout << get_name() << "\'s word: " << word1 << endl
 			 << p2.get_name() << "\'s word: " << word2 << endl;
 
-		// int counter = 0;
+		vector<char> charsToRemove;
 
-		//calculate equal letters
 		for(string::iterator it1 = word1.begin(); it1 != word1.end(); ++it1) {
 			for(string::iterator it2 = word2.begin(); it2 != word2.end(); ++it2) {
-				if((*it1) == (*it2)){
-					// counter++;
-					char c1 = *it1;
-					char c2 = *it2;
-					word1.erase(std::remove(word1.begin(), word1.end(), c1), word1.end());
-					word2.erase(std::remove(word2.begin(), word2.end(), c2), word2.end());
+				if((*it1 == *it2) and (*it1)){
+					// char c1 = *it1;
+					// char c2 = *it2;
+					// cout << "debug: it1 = " << c1 << ", it2 = " << c2 << endl;
+
+					charsToRemove.push_back(*it1);
 				}
 			}
 		}
 
+		//remove letters from words
+		for(vector<char>::iterator it = charsToRemove.begin(); it != charsToRemove.end(); ++it){
+			word1.erase(remove(word1.begin(), word1.end(), *it), word1.end());
+			word2.erase(remove(word2.begin(), word2.end(), *it), word2.end());
+		}
+
 		cout << "\nBOOM\n";
 		cout << "\nAftermath:\n" << get_name() << "\'s word: " << word1 << endl
-			 << p2.get_name() << "\'s word: " << word2 << endl;
-		//debug
-		// cout << "there are " << counter << " equal letters" << endl;
+			 << p2.get_name() << "\'s word: " << word2 << endl << endl;
+	
+		int nLetters1 = 0;
+		int nLetters2 = 0;
+
+		//count letters
+		for(string::iterator it1 = word1.begin(); it1 != word1.end(); ++it1)
+			nLetters1++;
+		for(string::iterator it2 = word2.begin(); it2 != word2.end(); ++it2)
+			nLetters2++;
+
+		// calc winner
+		if (nLetters1 > nLetters2){			//p1 wins
+			cout << get_name() << " wins with " << (nLetters1 - nLetters2) << " remaining letters more than " << p2.get_name() << endl;
+		}
+
+		else if(nLetters2 > nLetters1){		//p2 wins
+			cout << p2.get_name() << " wins with " << (nLetters2 - nLetters1) << " remaining letters more than " << get_name() << endl;
+
+		}
+
+		else if (nLetters1 == nLetters2){	//draw
+			cout << "It's a draw!" << endl;
+		}
+
+		else
+			cout << "error" << endl;
+
 	}
 
 private:
