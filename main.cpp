c6f9a2fdc34697987a73f7affb1f2994a44be0b7
Jonathan <jonathan.grangien@gmail.com>
2015-02-27 18:25:55 +0100

    Gamify main function

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "canon.h"
 
 int main(){
-	//add players
-	Canon Player1("jonathan");
-	Canon Player2("CPU");
 
-	cout << "debug: p1 points = " << Player1.get_points() << "\np2 points = " << Player2.get_points() << endl;
+	string exit = "";
+	string name;
+	int roundCounter = 1;
+
+	//add players
+	cout << "Your name: ";
+	cin >> name;
+
+	Canon Player1(name);
+	Canon Player2("CPU");
 
 	//list of words		TO DO: more fair, fixed number of letter words
 	string arr[] = {"because", "cause", "hello", "below", "hit", "miss", "rekt", "pwn", "combo", "jumbo", "critical", "optical", "isoenzyme",
@@ -17,10 +23,17 @@ int main(){
 		Player2.add_to_vec(arr[i]);
 	}
 
-	Player1.shoot(Player2);
+	//rounds
+	while(exit != "n"){
+		cout << "===========SCORE===========" << endl;
+		cout << Player1.get_name() << ": " << Player1.get_points() << "		" 
+			 << Player2.get_name() << ": " << Player2.get_points() << endl;
+		cout << "\n***ROUND " << roundCounter < "***\n";
+		
+		Player1.shoot(Player2);
 
-	cout << "debug: p1 points = " << Player1.get_points() << "\np2 points = " << Player2.get_points() << endl;
-
-
+		cout << "\nKeep playing? (y/n)" << endl;
+		cin >> exit;
+	}
 	return 0;
 }
